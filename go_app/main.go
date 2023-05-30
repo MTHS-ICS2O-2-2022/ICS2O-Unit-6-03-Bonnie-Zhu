f4eeea5839a8e2591edb8832cb7f224faa5d2640
Bonnie-Zhu <124389118+Bonnie-Zhu@users.noreply.github.com>
2023-05-30 17:08:07 +0000

    done

---
+++ go_app/main.go
@@ -0,0 +1,53 @@
+// Copyright (c) 2020 Bonnie Zhu All rights reserved
+//
+// Created by: Bonnie Zhu
+// Created on: May 2023
+// This file lets you check the weather
+
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+type WeatherData struct {
+	Main struct {
+		Temp float64 `json:"temp"`
+	} `json:"main"`
+	Weather []struct {
+		Icon string `json:"icon"`
+	} `json:"weather"`
+}
+
+func main() {
+	urlAddress := "https://api.openweathermap.org/data/2.5/weather?lat=45.4211435&lon=-75.6900574&appid=fe1d80e1e103cff8c6afd190cad23fa5"
+
+	response, err := http.Get(urlAddress)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer response.Body.Close()
+
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	var jsonData WeatherData
+	err = json.Unmarshal(body, &jsonData)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	temperature1 := jsonData.Main.Temp
+	temperature2 := temperature1 - 273.15
+
+	fmt.Println("The temperature outside is", fmt.Sprintf("%.2f", temperature2), "°C!")
+	fmt.Println("\nDone.")
+}
